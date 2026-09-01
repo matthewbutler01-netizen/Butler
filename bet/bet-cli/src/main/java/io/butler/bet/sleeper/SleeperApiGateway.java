5f05fba4dcf99bfd803b64a201e0480ec79f475e
Matt <323053860+matthewbutler01-netizen@users.noreply.github.com>
2026-09-01 02:03:15 -0500

    BF-007 implement Sleeper API gateway

---
+++ bet/bet-cli/src/main/java/io/butler/bet/sleeper/SleeperApiGateway.java
@@ -0,0 +1,40 @@
+package io.butler.bet.sleeper;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+final class SleeperApiGateway implements SleeperGateway {
+    private final SleeperClient client;
+    private final SleeperJsonParser parser;
+
+    SleeperApiGateway() {
+        this(new SleeperClient(), new SleeperJsonParser());
+    }
+
+    SleeperApiGateway(SleeperClient client, SleeperJsonParser parser) {
+        this.client = Objects.requireNonNull(client, "client must not be null");
+        this.parser = Objects.requireNonNull(parser, "parser must not be null");
+    }
+
+    @Override
+    public SleeperJsonParser.SleeperLeague fetchLeague(String leagueId) throws IOException, InterruptedException {
+        return parser.parseLeague(client.getLeague(leagueId));
+    }
+
+    @Override
+    public List<SleeperJsonParser.SleeperUser> fetchUsers(String leagueId) throws IOException, InterruptedException {
+        return parser.parseUsers(client.getLeagueUsers(leagueId));
+    }
+
+    @Override
+    public List<SleeperJsonParser.SleeperRoster> fetchRosters(String leagueId) throws IOException, InterruptedException {
+        return parser.parseRosters(client.getLeagueRosters(leagueId));
+    }
+
+    @Override
+    public Map<String, SleeperJsonParser.SleeperPlayer> fetchPlayers() throws IOException, InterruptedException {
+        return parser.parsePlayers(client.getNflPlayers());
+    }
+}
