6a3598b8ea25f14d4f71820125cc0ea7a9188655
Matt <323053860+matthewbutler01-netizen@users.noreply.github.com>
2026-09-01 01:30:56 -0500

    BF-006 add JSON parsing dependency

--- bet/bet-cli/build.gradle.kts
+++ bet/bet-cli/build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 
 dependencies {
     implementation("org.xerial:sqlite-jdbc:3.50.3.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.20.0")
     testImplementation(platform("org.junit:junit-bom:5.13.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
