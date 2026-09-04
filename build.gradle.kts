8a04e4f92e44ad25433ff44c6b4e92eb55a6baf1
Matt <323053860+matthewbutler01-netizen@users.noreply.github.com>
2026-09-04 01:23:31 -0500

    BF-344: Reject mutable dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,11 @@ plugins {
 allprojects {
     group = "io.butler"
     version = "0.1.0-SNAPSHOT"
+
+    configurations.configureEach {
+        resolutionStrategy {
+            failOnDynamicVersions()
+            failOnChangingVersions()
+        }
+    }
 }
