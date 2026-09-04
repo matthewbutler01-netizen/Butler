68ebb89a9b99274df75d4b97e14c09d4677c1c55
Matt <323053860+matthewbutler01-netizen@users.noreply.github.com>
2026-09-03 23:37:50 -0500

    BF-341: Upgrade Jackson Databind to 2.22.2 (#345)

--- bet/bet-cli/build.gradle.kts
+++ bet/bet-cli/build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 
 dependencies {
     implementation("org.xerial:sqlite-jdbc:3.53.4.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.20.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.22.2")
     testImplementation(platform("org.junit:junit-bom:5.13.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
