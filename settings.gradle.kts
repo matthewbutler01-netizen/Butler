17f1b2fbc5d6b5dd2f63ebc07e0369ce10b277ce
Matt <323053860+matthewbutler01-netizen@users.noreply.github.com>
2026-09-04 01:20:12 -0500

    BF-343: Centralize dependency repository policy

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,12 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
 rootProject.name = "Butler"
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+    }
+}
 
 include("bet:bet-cli")
