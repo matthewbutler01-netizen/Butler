31b351c8cb26a04f5a017b872cbd5fb846777eac
Matt <323053860+matthewbutler01-netizen@users.noreply.github.com>
2026-09-10 09:32:01 -0500

    BF-669 route app command through mutex guard

--- scripts/butler-app.cmd
+++ scripts/butler-app.cmd
@@ -1,3 +1,3 @@
 @echo off
-"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0butler-app.ps1" %*
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0butler-app-guard.ps1" %*
 exit /b %ERRORLEVEL%
