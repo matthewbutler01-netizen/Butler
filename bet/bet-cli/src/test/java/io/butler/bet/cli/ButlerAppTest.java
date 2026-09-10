d20356a9eefa5311771068f6b498748f81b4fb27
Matt <323053860+matthewbutler01-netizen@users.noreply.github.com>
2026-09-10 00:21:17 -0500

    BF-537: fix ranking readability test assertion import

--- bet/bet-cli/src/test/java/io/butler/bet/cli/ButlerAppTest.java
+++ bet/bet-cli/src/test/java/io/butler/bet/cli/ButlerAppTest.java
@@ -10,6 +10,7 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ButlerAppTest {
     @Test
